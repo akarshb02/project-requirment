e7bb613e1cba8422193baf10d1e49b8228bea94c
Akarsh <akarshb02@gmail.com>
2021-03-11 18:47:30 +0530

    creating link using react-router-link

---
+++ react/routers/pages/page.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+const page = () =>{
+
+    return (
+     <div>
+        <h1>welcome</h1>
+        <Link to="/component">click</Link>
+     </div>
+    )
+ 
+}
+
+export default page
